faeef9827601c74c6e94561423f80e30c7b0fd19
jpdougherty96 <jpdougherty96@gmail.com>
2026-06-01 20:17:55 -0500

    vercel analytics added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { VercelAnalytics } from '@/src/components/VercelAnalytics';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -19,6 +20,7 @@ export default function RootLayout() {
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
+      <VercelAnalytics />
     </ThemeProvider>
   );
 }
